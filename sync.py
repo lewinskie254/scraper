5f762a76c5797eea2b2539d2765a69e61934d7e0
lewinskie254 <machiulewis@gmail.com>
2026-05-23 20:31:04 +0300

    scrapped an entire website in just a few seconds

--- sync.py
+++ sync.py
@@ -1,45 +1,57 @@
 from playwright.sync_api import sync_playwright
 from bs4 import BeautifulSoup
-import json 
+import json
 from urllib.parse import urljoin
 import sys
+
 sys.stdout.reconfigure(encoding='utf-8')
 
 data = []
-with sync_playwright() as p: 
-    browser = p.chromium.launch(headless=False,  slow_mo=500)
+
+with sync_playwright() as p:
+    browser = p.chromium.launch(headless=False, slow_mo=300)
     page = browser.new_page()
 
     page.goto('https://books.toscrape.com/')
-    page.wait_for_selector('.col-xs-6', timeout=10000)    
-    html_content = page.content()
-    soup = BeautifulSoup(html_content, "lxml")
 
-    cards = soup.find_all(class_='col-xs-6')
+    while True:
+        page.wait_for_selector('.col-xs-6')
 
-    for i, card in enumerate(cards): 
-        title_div = card.find('h3')
-        title = title_div.a.text.strip()
-        price_div = card.find('div', class_='product_price')
-        price = price_div.find(class_='price_color').text.strip()
-        img_div = card.find("div", class_="image_container")
-        if not img_div:
-            continue
+        soup = BeautifulSoup(page.content(), "lxml")
+        cards = soup.find_all(class_='col-xs-6')
 
-        a_tag = img_div.find("a")
-        if not a_tag:
-            continue
+        for card in cards:
+            title = card.find('h3').a.text.strip()
+            price = card.find(class_='price_color').text.strip()
 
-        href = a_tag.get("href")
-        full_url = urljoin("https://books.toscrape.com/", href)
+            img_div = card.find("div", class_="image_container")
+            if not img_div:
+                continue
 
-        clean_map = {
-            'title' : title, 
-            'price' : price, 
-            'url' : full_url
-        }
-        data.append(clean_map)
+            a_tag = img_div.find("a")
+            if not a_tag:
+                continue
+
+            href = a_tag.get("href")
+            full_url = urljoin("https://books.toscrape.com/", href)
+
+            data.append({
+                "title": title,
+                "price": price,
+                "url": full_url
+            })
+
+        with open('books.json', 'w', encoding='utf-8') as file:
+            json.dump(data, file, indent=4, ensure_ascii=False)
+
+        next_btn = page.query_selector("li.next a")
+
+        if not next_btn:
+            break
+
+        next_url = next_btn.get_attribute("href")
+        page.goto(urljoin(page.url, next_url))
+
     browser.close()
 
-
-print(json.dumps(data, indent=4, ensure_ascii=False))
+print('--------Scraping Complete------------')
